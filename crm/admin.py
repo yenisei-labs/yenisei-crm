eacf3fef7807b7be0e878ffa02d2d3246e1d99b0
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-02-10 12:11:14 +0500

    Register new models in the admin panel

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DealList, Deal, Person
 
-# Register your models here.
+admin.site.register(DealList)
+admin.site.register(Deal)
+admin.site.register(Person)
