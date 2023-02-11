f90b264b72e90a8b26801768b318f73cb132ba3e
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-02-11 09:58:26 +0500

    Temporarily translate form labels into Russian
    The project needs to be brought to a working state as soon as possible,
    I will add localization later.

--- crm/forms.py
+++ crm/forms.py
@@ -4,34 +4,45 @@ from .models import DealList, Person
 
 
 class DealListForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=128)
+    title = forms.CharField(label='Заголовок', max_length=128)
+
 
 class DealForm(forms.Form):
     # Required fields
-    title = forms.CharField(max_length=128)
+    title = forms.CharField(label='Заголовок *', max_length=128)
     
     # Optional fields
-    note = forms.CharField(required=False, widget=forms.Textarea)
-    goods = forms.CharField(max_length=256, required=False)
+    note = forms.CharField(
+        label='Заметки',
+        required=False,
+        widget=forms.Textarea,
+    )
+    goods = forms.CharField(
+        label='Товар',
+        max_length=256,
+        required=False,
+    )
 
     # Money
-    income = forms.IntegerField(required=False)
-    goods_cost = forms.IntegerField(required=False)
-    loading_cost = forms.IntegerField(required=False)
-    delivery_cost = forms.IntegerField(required=False)
-    other_expenses = forms.IntegerField(required=False)
-    tax = forms.IntegerField(required=False)
+    income = forms.IntegerField(label='Доход', required=False)
+    goods_cost = forms.IntegerField(label='Стоимость товара', required=False)
+    loading_cost = forms.IntegerField(label='Стоимость погрузки', required=False)
+    delivery_cost = forms.IntegerField(label='Стоимость доставки', required=False)
+    other_expenses = forms.IntegerField(label='Прочие расходы', required=False)
+    tax = forms.IntegerField(label='Налог', required=False)
 
     # Delivery
-    delivery_date = forms.DateTimeField(required=False)
-    delivery_address = forms.CharField(max_length=256, required=False)
-    loading_address = forms.CharField(max_length=256, required=False)
+    delivery_date = forms.DateTimeField(label='Дата доставки', required=False)
+    delivery_address = forms.CharField(label='Адрес доставкик', max_length=256, required=False)
+    loading_address = forms.CharField(label='Адрес погрузки', max_length=256, required=False)
 
     buyer = forms.ModelChoiceField(
         Person.objects.all(),
+        label='Покупатель',
         required=False,
     )
     list = forms.ModelChoiceField(
         DealList.objects.all(),
+        label='Список',
         required=False,
     )
