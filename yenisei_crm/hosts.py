7f34d5d00c8d99fda0bef532b883c5305a85e88c
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-02-12 19:26:05 +0500

    Add scheme in CSRF protection configuration

--- yenisei_crm/hosts.py
+++ yenisei_crm/hosts.py
@@ -1,14 +1,16 @@
 import os
 
-def get_allowed_hosts() -> list[str]:
+def get_allowed_hosts(add_scheme = False) -> list[str]:
+    scheme = 'http://' if add_scheme else ''
     allowed_hosts = [
-        'localhost',
-        '127.0.0.1',
-        '::1',
+        scheme + 'localhost',
+        scheme + '127.0.0.1',
+        scheme + '::1',
     ]
 
     custom_host = os.environ.get("Y_CRM_HOST", None)
     if custom_host is not None:
+        url = 'https://' + custom_host if add_scheme else custom_host
         allowed_hosts.append(custom_host)
 
     return allowed_hosts
