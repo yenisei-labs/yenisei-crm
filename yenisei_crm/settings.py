73953213a569affb8389ea50c08b7d8e34ead928
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-02-12 17:49:07 +0500

    Configure static files root and whitenoise

--- yenisei_crm/settings.py
+++ yenisei_crm/settings.py
@@ -47,6 +47,7 @@ LOGIN_REDIRECT_URL = '/accounts/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/app/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
